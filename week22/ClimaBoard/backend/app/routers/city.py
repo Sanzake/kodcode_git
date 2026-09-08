72efad2b6f36b248ae374258f8b5395e43da26cb
Daniel <danhab20@gmail.com>
2026-09-08 17:07:58 +0300

    Add city router

---
+++ week22/ClimaBoard/backend/app/routers/city.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from services.meteo_getter import MeteoGetter
+
+router = APIRouter(prefix="/city")
+
+mg = MeteoGetter()
+
+
+@router.post("")
+def city(latitude: float, longitude: float):
+    current_temperature = mg.get_current_weather(latitude, longitude)
+    times, temperatures = mg.get_weather(latitude, longitude)
+
+    return {"current_temperature": current_temperature, "forecast": {"times": times, "temperature": temperatures}}
