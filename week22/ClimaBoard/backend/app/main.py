4c1b607caa80910aad515510cbe486c2e38d757a
Daniel <danhab20@gmail.com>
2026-09-08 16:52:39 +0300

    Add health and searchcity route to main

--- week22/ClimaBoard/backend/app/main.py
+++ week22/ClimaBoard/backend/app/main.py
@@ -1,11 +1,15 @@
+import uvicorn
 from fastapi import FastAPI
+from routers.health import router as health_router
+from routers.search_city import router as search_city_router
 from services.meteo_getter import MeteoGetter
 
 app = FastAPI()
 mg = MeteoGetter()
 
+app.include_router(health_router)
+app.include_router(search_city_router)
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello world"}
 
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
