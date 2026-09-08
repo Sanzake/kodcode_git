5014392049f7761f86f38876b378273687450ba2
Daniel <danhab20@gmail.com>
2026-09-08 16:07:14 +0300

    Add meteo getter

---
+++ week22/ClimaBoard/backend/app/services/meteo_getter.py
@@ -0,0 +1,88 @@
+import requests
+from config.config import CITY_URL, FORECAST_URL
+
+
+class MeteoGetter:
+    def __init__(self):
+        self.city_url = CITY_URL
+        self.forecast_url = FORECAST_URL
+
+    def search_city(self, city_name: str) -> list:
+        """
+        Get param city name and search in meteo matching cities
+        return list of 5 most matching cities
+
+        :city_name: str
+        :return: list
+        """
+        cities = []
+
+        fetchURL = self.city_url
+
+        params = {"count": 5, "language": "en", "format": "json"}
+        params["name"] = city_name
+
+        res = requests.get(fetchURL, params=params).json()
+        data = res.get("results", [])
+
+        for i in range(len(data)):
+            getted_id = data[i]["id"]
+            getted_city_name = data[i]["name"]
+            getted_country_name = data[i]["country"]
+            getted_latitude = data[i]["latitude"]
+            getted_longitude = data[i]["longitude"]
+            cities.append(
+                {
+                    "id": getted_id,
+                    "name": getted_city_name,
+                    "country": getted_country_name,
+                    "latitude": getted_latitude,
+                    "longitude": getted_longitude,
+                }
+            )
+        return cities
+
+    def get_weather(self, latitude: float, longitude: float) -> tuple:
+        """
+        Get wether for a week starting from current day
+        return tuple of 2 lists with time and temperature
+
+        :latitude: float
+        :longitude: float
+        :return: tuple(list, list)
+        """
+        fetchURL = self.forecast_url
+        params = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "hourly": "temperature_2m",
+        }
+
+        print(f"log[t.py] {fetchURL}")
+        print(f"log[t.py] {params}")
+
+        res = requests.get(fetchURL, params=params).json()
+        time = res["hourly"]["time"]
+        temperature_2m = res["hourly"]["temperature_2m"]
+
+        return (time, temperature_2m)
+
+    def get_current_weather(self, longitude: float, latitude: float) -> int:
+        """
+        Get current weather
+
+        :longitude: float
+        :latitude: float
+        :return: int
+        """
+        fetchURL = self.forecast_url
+        params = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "current": "temperature_2m",
+        }
+
+        res = requests.get(fetchURL, params=params).json()
+        temperature = res["current"]["temperature_2m"]
+
+        return temperature
