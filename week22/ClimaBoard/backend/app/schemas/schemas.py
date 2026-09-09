534a62bdea702715805b7136d582c3b42df4bf62
Daniel <danhab20@gmail.com>
2026-09-09 11:57:10 +0300

    Add schemas to depended file

---
+++ week22/ClimaBoard/backend/app/schemas/schemas.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel, Field
+
+
+class Coordinates(BaseModel):
+    latitude: float = Field(..., ge=-90.0, le=90.0)
+    longitude: float = Field(..., ge=-180.0, le=180.0)
+
+
+class CityName(BaseModel):
+    city_name: str = Field(..., min_length=2, max_length=50)
+
+
+class CurrentWeather(BaseModel):
+    temperature: float
+    wind: float
+    weather_code: int
+    apparent_temperature: float
+
+
+class City(BaseModel):
+    id: int
+    name: str
+    country: str
+    latitude: float
+    longitude: float
+
+
+class ForecastWeather(BaseModel):
+    dates: list[str]
+    min_temperatures: list[float]
+    max_temperatures: list[float]
+    max_winds_speed: list[float]
+    weather_code: list[int]
+    min_apparent: list[float]
+    max_apparent: list[float]
