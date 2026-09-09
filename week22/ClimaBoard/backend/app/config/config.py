8ae2fc398b2431f0202bd8faf5f7ae4d6b3ce993
Daniel <danhab20@gmail.com>
2026-09-09 09:16:15 +0300

    Add host and port

--- week22/ClimaBoard/backend/app/config/config.py
+++ week22/ClimaBoard/backend/app/config/config.py
@@ -1,2 +1,5 @@
 CITY_URL = "https://geocoding-api.open-meteo.com/v1/search"
 FORECAST_URL = "https://api.open-meteo.com/v1/forecast"
+
+HOST = "127.0.0.1"
+PORT = 8000
--- week22/ClimaBoard/backend/app/main.py
+++ week22/ClimaBoard/backend/app/main.py
@@ -1,5 +1,7 @@
 import uvicorn
+from config.config import HOST, PORT
 from fastapi import FastAPI
+from routers.city import router as city
 from routers.health import router as health_router
 from routers.search_city import router as search_city_router
 from services.meteo_getter import MeteoGetter
@@ -9,6 +11,7 @@ mg = MeteoGetter()
 
 app.include_router(health_router)
 app.include_router(search_city_router)
+app.include_router(city)
 
 
 # mg = MeteoGetter()
@@ -19,4 +22,4 @@ app.include_router(search_city_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", host=HOST, port=PORT, reload=True)
