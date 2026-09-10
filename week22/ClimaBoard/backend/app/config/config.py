0863f4a4d031b5df45be46ed12d05618a41177ac
Daniel <danhab20@gmail.com>
2026-09-10 09:42:59 +0300

    Refactor meteo getter

--- week22/ClimaBoard/backend/app/config/config.py
+++ week22/ClimaBoard/backend/app/config/config.py
@@ -3,6 +3,10 @@ from pathlib import Path
 CITY_URL = "https://geocoding-api.open-meteo.com/v1/search"
 FORECAST_URL = "https://api.open-meteo.com/v1/forecast"
 
+CITIES_TO_GET = 5
+
+TIMEOUT = 10
+
 HOST = "127.0.0.1"
 PORT = 8000
 
--- week22/ClimaBoard/backend/app/services/meteo_getter.py
+++ week22/ClimaBoard/backend/app/services/meteo_getter.py
@@ -1,5 +1,5 @@
 import requests
-from config.config import CITY_URL, FORECAST_URL
+from config.config import CITIES_TO_GET, CITY_URL, FORECAST_URL, TIMEOUT
 from schemas.schemas import City, CurrentWeather, ForecastWeather
 
 
@@ -14,34 +14,27 @@ class MeteoGetter:
         return list of 5 most matching cities
 
         :city_name: str
-        :return: list
+        :return: list[City]
         """
-        cities = []
+        fetch_url = self.city_url
 
-        fetchURL = self.city_url
-
-        params = {"count": 5, "language": "en", "format": "json"}
+        params = {"count": CITIES_TO_GET, "language": "en", "format": "json"}
         params["name"] = city_name
 
-        res = requests.get(fetchURL, params=params).json()
-        data = res.get("results", [])
+        res = requests.get(fetch_url, params=params, timeout=TIMEOUT).json()
+        data: list[dict] = res.get("results", [])
 
-        for i in range(len(data)):
-            getted_id: int = data[i]["id"]
-            getted_city_name: str = data[i]["name"]
-            getted_country_name: str = data[i]["country"]
-            getted_latitude: float = data[i]["latitude"]
-            getted_longitude: float = data[i]["longitude"]
-
-            cities.append(
-                City(
-                    id=getted_id,
-                    name=getted_city_name,
-                    country=getted_country_name,
-                    latitude=getted_latitude,
-                    longitude=getted_longitude,
-                )
+        cities = [
+            City(
+                id=city.get("id"),
+                name=city.get("name"),
+                country=city.get("country"),
+                latitude=city.get("latitude"),
+                longitude=city.get("longitude"),
             )
+            for city in data
+        ]
+
         return cities
 
     def get_forecast_weather(
@@ -49,42 +42,31 @@ class MeteoGetter:
     ) -> ForecastWeather:
         """
         Get wether for a week starting from current day
-        return tuple of 2 lists with time and temperature
 
         :latitude: float
         :longitude: float
-        :return: tuple(list, list)
+        :return: ForecastWeather
         """
-        fetchURL = self.forecast_url
+        fetch_url = self.forecast_url
         params = {
             "latitude": latitude,
             "longitude": longitude,
             "daily": "weather_code,wind_speed_10m_max,temperature_2m_min,temperature_2m_max,apparent_temperature_max,apparent_temperature_min,sunrise,sunset",
         }
 
-        res = requests.get(fetchURL, params=params).json()
-        res_daily = res["daily"]
-
-        dates = res_daily["time"]
-        min_temperatures = res_daily["temperature_2m_min"]
-        max_temperatures = res_daily["temperature_2m_max"]
-        max_winds_speed = res_daily["wind_speed_10m_max"]
-        weather_code = res_daily["weather_code"]
-        min_apparent = res_daily["apparent_temperature_min"]
-        max_apparent = res_daily["apparent_temperature_max"]
-        sunrise = res_daily["sunrise"]
-        sunset = res_daily["sunset"]
+        res = requests.get(fetch_url, params=params, timeout=TIMEOUT).json()
+        res_daily: dict = res.get("daily", {})
 
         forecast_weather = ForecastWeather(
-            dates=dates,
-            min_temperatures=min_temperatures,
-            max_temperatures=max_temperatures,
-            max_winds_speed=max_winds_speed,
-            weather_code=weather_code,
-            min_apparent=min_apparent,
-            max_apparent=max_apparent,
-            sunrise=sunrise,
-            sunset=sunset
+            dates=res_daily.get("time"),
+            min_temperatures=res_daily.get("temperature_2m_min"),
+            max_temperatures=res_daily.get("temperature_2m_max"),
+            max_winds_speed=res_daily.get("wind_speed_10m_max"),
+            weather_code=res_daily.get("weather_code"),
+            min_apparent=res_daily.get("apparent_temperature_min"),
+            max_apparent=res_daily.get("apparent_temperature_max"),
+            sunrise=res_daily.get("sunrise"),
+            sunset=res_daily.get("sunset"),
         )
 
         return forecast_weather
@@ -95,32 +77,25 @@ class MeteoGetter:
 
         :longitude: float
         :latitude: float
-        :return: int
+        :return: CurrentWeather
         """
-        fetchURL = self.forecast_url
+        fetch_url = self.forecast_url
         params = {
             "latitude": latitude,
             "longitude": longitude,
             "current": "temperature_2m,wind_speed_10m,weather_code,apparent_temperature,wind_direction_10m,rain",
         }
 
-        res = requests.get(fetchURL, params=params).json()
-        res_current = res["current"]
-
-        temperature: str = res_current["temperature_2m"]
-        wind: str = res_current["wind_speed_10m"]
-        weather_code: str = res_current["weather_code"]
-        apparent_temperature: str = res_current["apparent_temperature"]
-        wind_direction = res_current["wind_direction_10m"]
-        rain = res_current["rain"]
+        res = requests.get(fetch_url, params=params, timeout=TIMEOUT).json()
+        res_current: dict = res.get("current", {})
 
         current_weather = CurrentWeather(
-            temperature=temperature,
-            wind=wind,
-            weather_code=weather_code,
-            apparent_temperature=apparent_temperature,
-            wind_direction=wind_direction,
-            rain=rain
+            temperature=res_current.get("temperature_2m"),
+            wind=res_current.get("wind_speed_10m"),
+            weather_code=res_current.get("weather_code"),
+            apparent_temperature=res_current.get("apparent_temperature"),
+            wind_direction=res_current.get("wind_direction_10m"),
+            rain=res_current.get("rain"),
         )
 
         return current_weather
