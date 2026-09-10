4206dfef68ad1d20b5c4f90f6b308866e86728cd
Daniel <danhab20@gmail.com>
2026-09-10 10:06:51 +0300

    More resistance to favorites manager

--- week22/ClimaBoard/backend/app/services/favorites_manager.py
+++ week22/ClimaBoard/backend/app/services/favorites_manager.py
@@ -10,22 +10,25 @@ class FavoritesManager:
 
 
     def get_data_from_json(self) -> dict:
-        data = json.loads(self.favorites_path.read_text())
-        return data
+        if not self.favorites_path.exists():
+            return {}
+        try:
+            data = json.loads(self.favorites_path.read_text(encoding="UTF-8"))
+            return data
+        except json.JSONDecodeError:
+            return {}
 
 
     def write_data_to_json(self, data: dict):
         json_string = json.dumps(data, indent=2)
-        self.favorites_path.write_text(json_string)
+        self.favorites_path.write_text(json_string, encoding="UTF-8")
 
 
     def get_favorites_from_json(self, username):
         data = self.get_data_from_json()
 
-        if username in data and len(data[username]) > 0:
-            return data[username]
-
-        return {"message": f"No favorites for {username}"}
+        
+        return data.get(username, [])
 
 
     def add_favorite_to_json(self, body: BodyFavoriteAdd):
@@ -34,15 +37,15 @@ class FavoritesManager:
 
         data = self.get_data_from_json()
 
-        if username in data:
-            users_cities_id = [city.get("id") for city in data[username]]
-            if city["id"] not in users_cities_id:
-                data[username].append(city)
-        else:
-            data[username] = [city]
+        if username not in data:
+            data[username] = []
+
+        users_cities_id = [c.get("id") for c in data[username]]
+        if city["id"] not in users_cities_id:
+            data[username].append(city)
 
         self.write_data_to_json(data)
-        return {"message": "Added succesfully!"}
+        return {"message": "Added successfully!"}
 
 
     def remove_from_favorite(self, body: BodyFavoriteRemove):
@@ -61,4 +64,4 @@ class FavoritesManager:
         data[username] = new_users_cities
 
         self.write_data_to_json(data)
-        return {"message": "Removed succesfully!"}
+        return {"message": "Removed successfully!"}
