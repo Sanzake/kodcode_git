a35e5a0fbaf619b82ef6527d7751a3156d975fb9
Daniel <danhab20@gmail.com>
2026-09-14 11:05:50 +0300

    Add backround and add styles to homePage

--- week22/ClimaBoard/frontend/src/pages/HomePage/HomePage.tsx
+++ week22/ClimaBoard/frontend/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import { useRef } from "react"
 import { useCurrentUserStore } from "../../store/currentUsername"
+import "./HomePage.css"
 
 export default function HomePage() {
     const username = useCurrentUserStore((s) => s.currentUser)
@@ -11,15 +12,20 @@ export default function HomePage() {
     const sendUsername = (e: React.SubmitEvent<HTMLFormElement>) => {
         e.preventDefault()
         const newUsername = inputRef.current?.value
-        if (newUsername && newUsername.length > 0) setUsername(newUsername)
+        if (newUsername && newUsername.length > 0) {
+            setUsername(newUsername)
+            if (inputRef.current) {
+                inputRef.current.value = ""
+            }
+        }
     }
     return (
-        <div>
+        <div className="homePage">
             <h1>Welcome, {username}!</h1>
             <h2>This app allows you to check weather!</h2>
-            <form onSubmit={sendUsername}>
-                <input type="text" placeholder="Your username..." ref={inputRef}/>
-                <button type="submit">send</button>
+            <form className="usernameForm" onSubmit={sendUsername}>
+                <input className="usernameInput" type="text" placeholder="Your username..." ref={inputRef}/>
+                <button className="usernameButton" type="submit">send</button>
             </form>
         </div>
     )
